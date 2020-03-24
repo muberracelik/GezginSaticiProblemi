55a9d3d0fb23b6eb8e8218c089b975bb6c56559a
Müberra Çelik <muberracelik@icloud.com>
2020-03-24 22:40:35 +0300

    Proje oluşturuldu
    sınıflar ve belirli metotlar eklendi

---
+++ src/Dijkstra.java
@@ -0,0 +1,80 @@
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Edge { // edge => kenar
+
+    public final Vertex hedefVertex;
+    public final double mesafe;
+
+    public Edge(Vertex hedefVertex, double mesafe) {
+        this.hedefVertex = hedefVertex;
+        this.mesafe = mesafe;
+    }
+}
+
+class Vertex { // vertex => düğüm
+
+    public String dugumIsmi;
+    public int plaka;
+    public ArrayList<Edge> komsular = new ArrayList<>();
+    public double minMesafe = Double.POSITIVE_INFINITY;
+    public boolean kullanildiMi = false;
+
+    public Vertex(String sehirİsmi, int sehirPlaka) {
+        dugumIsmi = sehirİsmi;
+        plaka = sehirPlaka;
+    }
+
+}
+
+public class Dijkstra {
+
+    public static ArrayList<Vertex> sehirler = new ArrayList<>();
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        FileReader komsularDosyasi = new FileReader("komsuluk.txt");
+        Scanner scanner = new Scanner(komsularDosyasi);
+
+        while (scanner.hasNextLine()) {
+            String satir = scanner.nextLine();
+            String[] bolum = satir.split(",");
+            String[] ikili = bolum[0].split("-");
+            Integer plaka = Integer.parseInt(ikili[0]);//plakayı integera çevirmemiz lazım
+            sehirler.add(new Vertex(ikili[1], plaka));
+
+        }
+        komsularDosyasi.close();
+
+        komsularDosyasi = new FileReader("komsuluk.txt");
+        scanner = new Scanner(komsularDosyasi);
+        int sehirCursor = 0;
+        while (scanner.hasNextLine()) {
+            String satir = scanner.nextLine();
+            String[] bolum = satir.split(",");
+
+            for (int i = 1; i < bolum.length; i++) {//tüm komşulukları dolaşması için
+                String cursorBolum = bolum[i].trim();
+                String[] ikili = cursorBolum.split("-");
+                int plaka = Integer.parseInt(ikili[0]);
+                double mesafe = Double.parseDouble(ikili[1]);
+                sehirler.get(sehirCursor).komsular.add(new Edge(sehirler.get(plaka - 1), mesafe));
+
+            }
+            sehirCursor++;
+        }
+        komsularDosyasi.close();
+
+        sehirler.get(40).minMesafe = 0;
+        for (int i = 0; i < sehirler.get(40).komsular.size(); i++) {
+            sehirler.get(40).komsular.get(i).hedefVertex.minMesafe = sehirler.get(40).komsular.get(i).mesafe;
+        }
+       //  System.out.println(sehirler.get(40).komsular.get(0).hedefVertex.plaka+"   "+sehirler.get(40).komsular.get(0).hedefVertex.minMesafe);
+      
+       
+     
+    }
+}
