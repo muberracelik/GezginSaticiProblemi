634afbd9d70b0e10d9d2d29ebc6b4410a0cada24
Müberra Çelik <muberracelik@icloud.com>
2020-03-26 23:28:18 +0300

    Dijkstra fonksiyonu tamamlandı

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -70,26 +70,46 @@ public class Dijkstra {
         komsularDosyasi.close();
         Vertex aktifSehir = sehirler.get(40);
         sehirler.get(40).minMesafe = 0;
-        for (int i = 0; i < sehirler.get(40).komsular.size(); i++) {
+        for (int i = 0; i < sehirler.get(40).komsular.size(); i++) {//kocaelinin komslarının mesafelerini ve parentlerini güncelledik
             sehirler.get(40).komsular.get(i).hedefVertex.minMesafe = sehirler.get(40).komsular.get(i).mesafe;
-            sehirler.get(40).komsular.get(i).hedefVertex.parent=aktifSehir;
+            sehirler.get(40).komsular.get(i).hedefVertex.parent = aktifSehir;
         }
         //  System.out.println(sehirler.get(40).komsular.get(0).hedefVertex.plaka+"   "+sehirler.get(40).komsular.get(0).hedefVertex.minMesafe);
-        int tmp1 = 26;
-// sıradaki sehri secme fonkk<<<<<<<<<<<<<<<<
+        int aktifSehirIndex = 26;//bunu 26 yapmamızın sebebi: bir şehrin maks 13 tane komşusu vardır.
+
         double tmp = Double.POSITIVE_INFINITY;
-        for (int i = 0; i < sehirler.size(); i++) {
+        for (int i = 0; i < sehirler.size(); i++) {// sıradaki şehri belirler, şehrin kullanılıp kullanılmadığına da bakar.
             if (sehirler.get(i).minMesafe < tmp && sehirler.get(i).kullanildiMi != true && i != (aktifSehir.plaka - 1)) {
-                tmp = sehirler.get(i).minMesafe;
-                tmp1 = i;
+                tmp = sehirler.get(i).minMesafe;    //minimum mesafeyi veriyor(kontrol amaçlı)
+                aktifSehirIndex = i;   //şehrin indeksini tutar                
             }
         }
-        aktifSehir = sehirler.get(tmp1);
+        aktifSehir = sehirler.get(aktifSehirIndex);
 
-        System.out.println(aktifSehir.minMesafe + " " + (tmp1 + 1));
-        //<<<<<<<<<<<<<
-       for(int i=0;i<(sehirler.size()-1);i++){
-        
-       }
+        System.out.println(aktifSehir.minMesafe + " " + (aktifSehirIndex + 1));
+        System.out.println(aktifSehir.dugumIsmi);
+
+        for (int i = 0; i < (sehirler.size() - 1); i++) {
+            aktifSehir.kullanildiMi = true;
+            for (int j = 0; j < sehirler.get(aktifSehirIndex).komsular.size(); j++) {//kocaelinin komslarının mesafelerini ve parentlerini güncelledik
+                if (sehirler.get(aktifSehirIndex).komsular.get(j).hedefVertex.minMesafe > (sehirler.get(aktifSehirIndex).komsular.get(j).mesafe + aktifSehir.minMesafe)) {
+
+                    sehirler.get(aktifSehirIndex).komsular.get(j).hedefVertex.minMesafe = (sehirler.get(aktifSehirIndex).komsular.get(j).mesafe + aktifSehir.minMesafe);
+                    sehirler.get(aktifSehirIndex).komsular.get(j).hedefVertex.parent = aktifSehir;
+                }
+            }
+
+            tmp = Double.POSITIVE_INFINITY;
+
+            for (int k = 0; k < sehirler.size(); k++) {// sıradaki şehri belirler, şehrin kullanılıp kullanılmadığına da bakar.
+                if (sehirler.get(k).minMesafe < tmp && sehirler.get(k).kullanildiMi != true && k != (aktifSehir.plaka - 1)) {
+                    tmp = sehirler.get(k).minMesafe;    //minimum mesafeyi veriyor(kontrol amaçlı)
+                    aktifSehirIndex = k;   //şehrin indeksini tutar                
+                }
+            }
+            aktifSehir = sehirler.get(aktifSehirIndex);
+        }
+        System.out.println(sehirler.get(25).parent.parent.parent.dugumIsmi);
     }
+    
 }
